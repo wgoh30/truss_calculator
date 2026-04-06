9fc28c65eac742da14a3aeee7da232aafa87b308
wgoh30 <wgoh0034@student.monash.edu>
2026-04-06 23:28:48 +0800

    Added current design

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ version date: 05 Apr 2026
 """
 from utils.setup_functions import setup
 from utils.calculation_functions import solve_axial_force
-from numpy import printoptions
+from numpy import printoptions, degrees
 from visualiser.truss_visualiser import draw_truss
 from visualiser.visualiser_setup import turtle_setup
 
@@ -19,6 +19,6 @@ def main() -> None:
     # print out the output
     with printoptions(suppress=True, precision=3):
         for iteration in range(len(setupDict['force_matrix'])):
-            print(f"{setupDict['force_list'][iteration].name}: {solvedForces[iteration][0]:.2f} N")
+            print(f"{setupDict['force_list'][iteration].name}: {solvedForces[iteration][0]:.2f} N, {degrees(setupDict['force_list'][iteration].angle):.2f} deg")
 if __name__ == "__main__":
     main()
